285356905b520495a9072d96f250ec4a7623ecfb
Lotusei <tinatretjakova123@gmail.com>
2025-09-18 15:38:37 +0300

    jauns

--- Sakums.py
+++ Sakums.py
@@ -1,3 +1,49 @@
-print("Hello")
-print("Bye")
-print("hello again")
+import tkinter as tk
+
+
+logs = tk.Tk()
+logs.title("Loga elementi")
+logs.geometry("300x300+1100+500")
+
+# teksts_1 = tk.Label(logs, text = '''Labie darbi!  Dari vienmēr labu!''',
+#     bg ="#B8769C",
+#     fg ="#77E7A9",
+
+#     font=('Arial',10,'bold'),
+#     padx=20,
+#     pady=20,
+
+#     width=13,
+#     height=3,
+
+#     anchor="ne",
+
+#     relief=tk.RAISED,
+#     bd=10,
+
+#     justify=tk.CENTER
+#     )
+
+def teksta_lauks1():
+    tekstsLoga1=tk.Label(logs, text='Spoži spīd saule')
+    tekstsLoga1.pack()
+
+poga2 = tk.Button(logs, text='vasara',
+                 command=teksta_lauks1
+                 )
+poga2.pack()
+
+exit_button= tk.Button(
+    logs,
+    text="exit",
+    command=lambda: logs.quit()
+)
+exit_button.pack()
+
+# teksts_1.pack()
+
+
+logs.minsize(50,50)
+logs.maxsize(500,500)
+logs.resizable(True,True)
+logs.mainloop()
