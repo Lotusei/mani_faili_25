2ab8e5ab3103f64a45690e08d228f6d76ebfa6ed
Lotusei <tinatretjakova123@gmail.com>
2025-11-13 15:36:34 +0200

    Klases patstāvīgais darbs

--- Klases.py
+++ Klases.py
@@ -50,21 +50,83 @@
 
 # -------------------------------------------------------------------------------------------------------------------------------------------
 
-from datetime import date
+# from datetime import date
 
-class Persona:
-    def __init__(self, vards, valsts, dzgads):
-        self.vards = vards
-        self.valsts = valsts
-        self.dzgads = dzgads
-    def vecums(self):
-        print(self.vards + " no " + self.valsts + "s ir" , date.today().year - self.dzgads.year , "gadus vecs/a.")
+# class Persona:
+#     def __init__(self, vards, valsts, dzgads):
+#         self.vards = vards
+#         self.valsts = valsts
+#         self.dzgads = dzgads
+#     def vecums(self):
+#         print(self.vards + " no " + self.valsts + "s ir" , date.today().year - self.dzgads.year , "gadus vecs/a.")
 
 
-P1 = Persona("Perrijs", "Krievija", date(1638, 12, 25))
+# P1 = Persona("Perrijs", "Krievija", date(1638, 12, 25))
 
-print(P1.vards)
-print(P1.valsts)
-print(P1.dzgads)
+# print(P1.vards)
+# print(P1.valsts)
+# print(P1.dzgads)
 
-P1.vecums()
+# P1.vecums()
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# class Student:
+#     def __init__(self, vards, atzime, atzime1):
+#         self.vards = vards
+#         self.atzime = atzime
+#         self.atzime1 = atzime1
+
+#     def avarage_grade(self):
+#         return ( self.atzime1 + self.atzime)/2 
+    
+#     def show_info(self):
+#         print(f"{ self.vards } vidējā atzīme ir { self.avarage_grade() }")
+
+
+
+# Student1 = Student("Perrijs", 6, 7)
+
+# Student1.show_info()
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# def calculate_average(atzimes):
+#     return sum(atzimes / len(atzimes))
+
+# class Student:
+#     def __init__(self, vards, atzimes):
+#         self.vards = vards
+#         self.atzimes = atzimes
+
+#     def max_grade(self):
+#         return max(self.atzimes)
+    
+#     def is_excelent(self):
+#         avg = calculate_average(self.grades)
+#         if avg >= min_avg:
+#             print(self.vards, "saņems Zelta piparkūku, jo vidējā atzīme ir ", avg)
+#         else:
+#             print(self.vards, "nesaņems Zelta piparkūku, jo vidējā atzīme ir ", avg)
+
+
+# Student1 = Student("Perrijs", [6,7,9,10,10])
+
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# from datetime import date
+
+# class Transports:
+#     def __init__(self, marka, maxAtrums, nobraukums, gads):
+#         self.marka = marka
+#         self.maxAtrums = maxAtrums
+#         self.nobraukums = nobraukums
+#         self.gads = gads
+#         self.atrums = 0
+
+#     def avarage_speed(self):
+#         return date.today().year - int(self.gads)
+
+# class Autobuss:
+#     pass
---
+++ Klases_Tina.py
@@ -0,0 +1,118 @@
+# 1. uzd ----------------------------------------------------------------------------------------------------------
+
+# class Darbinieks:
+#     def __init__(self, vards, amats):
+#         self.vards = vards
+#         self.amats = amats
+
+
+# jauns_darbinieks = Darbinieks("Anna","Projekta vadītāja")
+
+# print(jauns_darbinieks.vards)
+# print(jauns_darbinieks.amats)
+
+# 2. uzd ----------------------------------------------------------------------------------------------------------
+
+# class Darbinieks:
+#     def __init__(self, vards, amats):
+#         self.vards = vards
+#         self.amats = amats
+#     def radit_profilu(self):
+#         print("Darbinieks: " + self.vards + ", Amats: " + self.amats)
+
+
+# jauns_darbinieks = Darbinieks("Anna","Projekta vadītāja")
+
+# jauns_darbinieks.radit_profilu()
+
+# 3. uzd ----------------------------------------------------------------------------------------------------------
+
+# from tkinter import messagebox
+# class Darbinieks:
+#     def __init__(self, vards, amats, alga = 3000):
+#         self.vards = vards
+#         self.amats = amats
+#         self.alga = alga
+
+#     def paaugstinat_algu(self, summa):
+#         self.alga += summa
+#         messagebox.showinfo("Paaugstinājums!", f"Jaunā alga ir: {self.alga}")      
+
+#     def radit_profilu(self):
+#         print("Darbinieks: " + self.vards + ", Amats: " + self.amats + ", Alga:" , self.alga)
+
+
+
+
+# jauns_darbinieks = Darbinieks("Anna","Projekta vadītāja")
+
+
+
+# jauns_darbinieks.paaugstinat_algu(500)
+
+# jauns_darbinieks.radit_profilu()
+
+# 4.uzd ------------------------------------------------------------------------------------------------------------------
+
+# from tkinter import messagebox
+# class Darbinieks:
+#     def __init__(self, vards, amats, alga = 2000):
+#         self.vards = vards
+#         self.amats = amats
+#         self.alga = alga
+
+#     def paaugstinat_algu(self, summa):
+#         self.alga += summa
+#         messagebox.showinfo("Paaugstinājums!", f"Jaunā alga ir: {self.alga}")      
+         
+#     def radit_profilu(self):
+#         print("Darbinieks: " + self.vards + ", Amats: " + self.amats + ", Alga:" , self.alga)
+
+
+
+
+# jauns_darbinieks = Darbinieks("Anna","Projekta vadītāja")
+
+# vecs_darbinieks = Darbinieks("Jānis","Analītiķis")
+
+
+
+# jauns_darbinieks.paaugstinat_algu(500)
+
+# jauns_darbinieks.radit_profilu()
+
+# vecs_darbinieks.radit_profilu()
+
+# 5.uzd ------------------------------------------------------------------------------------------------------------------
+
+from tkinter import messagebox
+class Darbinieks:
+    def __init__(self, vards, amats, alga = 2000):
+        self.vards = vards
+        self.amats = amats
+        self.alga = alga
+
+    def paaugstinat_algu(self, summa):
+
+        if summa > 0:
+            self.alga += summa
+            messagebox.showinfo("Paaugstinājums!", f"Jaunā alga ir: {self.alga}")    
+        else:
+            messagebox.showwarning("Kļūda!","Algas paaugstinājums nevar būt negatīvs vai nulle")
+
+         
+    def radit_profilu(self):
+        print("Darbinieks: " + self.vards + ", Amats: " + self.amats + ", Alga:" , self.alga)
+
+
+
+
+jauns_darbinieks = Darbinieks("Anna","Projekta vadītāja")
+
+vecs_darbinieks = Darbinieks("Jānis","Analītiķis")
+
+
+
+jauns_darbinieks.paaugstinat_algu(200)
+
+jauns_darbinieks.paaugstinat_algu(-100)
